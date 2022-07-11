d25ee03bfa59ae91e6c6ed6832a824a92d631088
Ali-AliAli-Ali <alivolf@icloud.com>
2022-07-11 12:57:50 +0300

    Problem 3.1 solved

--- hmwrk1.js
+++ hmwrk1.js
@@ -1,16 +1,30 @@
 //1
-function newmap(arr, func) {
+/*function newmap(arr, func) {
     arr.forEach((elem, index) => arr[index] = func(arr[index]));
     return arr;
 }
 function randfunc1(t) { return ++t; } 
 function randfunc2(t) { return Math.sqrt(t); } 
 function randfunc3(t) { return t + " parrots"; } 
-function randfunc4(t) { return "E"; } 
 
 
 const array = [1, 3, 24]
 console.log(newmap(array, randfunc1));
 console.log(newmap(array, randfunc2));
-console.log(newmap(array, randfunc3));
-console.log(newmap(array, randfunc4));
+console.log(newmap(array, randfunc3));*/
+
+
+//3
+const arr = [1, 2, 5, 3, 3, 5, 2, 1, 4];
+
+    //(1)
+const newarr = [];
+
+function is_in_newarr(elem) {
+    let elemi = newarr.indexOf(elem);
+    (elemi === -1) ? newarr.push(elem) : newarr.splice(elemi, 1);
+}
+
+arr.forEach(elem => is_in_newarr(elem));
+console.log(newarr);
+
