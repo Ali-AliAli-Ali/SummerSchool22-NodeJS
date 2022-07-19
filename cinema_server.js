8bdb86ca6bc752c3dad6b29aa4eb7789663a9242
Ali-AliAli-Ali <alivolf@icloud.com>
2022-07-19 08:37:19 +0300

    Basic server built

--- cinema_server.js
+++ cinema_server.js
@@ -1,1 +1,12 @@
-console.log("server is in process");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/ping") 
+        return res.end("Yours is pretty low");
+
+    res.statusCode = 404;
+    return res.end("Not found");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => { console.log(`server is on port ${PORT}`); })
