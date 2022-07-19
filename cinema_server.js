98db06f1db219bfae305362b56fe9ad815555f2f
Ali-AliAli-Ali <alivolf@icloud.com>
2022-07-19 15:41:04 +0300

    Start and nodemon scripts added and tested

--- cinema_server.js
+++ cinema_server.js
@@ -9,6 +9,7 @@ const server = http.createServer((req, res) => {
     if (req.url === "/ping") {
         res.setHeader("Content-type", "text/html")
         res.write("<h1>Going to make a joke: \n\n</h1>")
+        res.write("<h1>Going: \n\n</h1>")
         return res.end("Yours is pretty low");
     }
 
@@ -16,4 +17,4 @@ const server = http.createServer((req, res) => {
     return res.end("Not found");
 });
 
-server.listen(PORT, () => { console.log(`server is on port ${PORT}`); })
+server.listen(PORT, () => { console.log(`server is on port ${PORT}`); });
