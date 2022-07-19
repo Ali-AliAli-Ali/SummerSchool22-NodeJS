6d1dbee566348330687d452dab61ab5a9eccb86c
Ali-AliAli-Ali <alivolf@icloud.com>
2022-07-19 15:30:43 +0300

    New packages loaded

--- cinema_server.js
+++ cinema_server.js
@@ -1,12 +1,19 @@
 const http = require("http");
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/ping") 
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.headers);
+
+    if (req.url === "/ping") {
+        res.setHeader("Content-type", "text/html")
+        res.write("<h1>Going to make a joke: \n\n</h1>")
         return res.end("Yours is pretty low");
+    }
 
     res.statusCode = 404;
     return res.end("Not found");
 });
 
-const PORT = 3000;
 server.listen(PORT, () => { console.log(`server is on port ${PORT}`); })
